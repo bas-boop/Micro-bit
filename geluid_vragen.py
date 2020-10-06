3c924f1397eb34b1a2ec1548a8465f297ef93063
bas-boop <70578065+bas-boop@users.noreply.github.com>
2020-10-06 13:09:11 +0200

    geluid vragen toegevoegd
    dit zijn de 3 vragen over geluid die ik heb beantwoord.

---
+++ geluid_vragen.py
@@ -0,0 +1,4 @@
+print("Dit zijn mijn antwoorden van de 3 vragen over geluid.")
+print("Antwoord 1: het is een toon dat langer duudt dan een hoge toon.\n")
+print("Antwoord 2: 120.000 hertz.\n")
+print("Antwoord 3: ja natuurlijk, als ik 8 uur in stadslawaai zit of bij een concert bij dat 120 dB gebruikt.\n")
